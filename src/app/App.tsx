fb82e4fed8c362177cf82641279f1b0c0ce731ec
a.popenkov <a.popenkov@thecoders.ru>
2023-09-29 15:37:17 +0300

    starter added

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import React, { Suspense, useEffect } from 'react';
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+
+function App() {
+  const { theme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
